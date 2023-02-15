112500d4310aa7c72fb6475ab20be96427e3f2d7
Joaquin <j.penuela@uniandes.edu.co>
2023-02-15 11:40:37 -0500

    Deleting checkpoints files

--- delphes_reader/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,5 +0,0 @@
-__version__ = "0.0.1"
-
-from delphes_reader.loader import DelphesLoader
-from delphes_reader.root_analysis import Quiet
-
--- ml_tools/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-__version__ = "0.0.1"
-
-from ml_tools.log_reg_classifier import Log_Reg_Classifier
-from ml_tools.xgb_classifier import XGB_Classifier
