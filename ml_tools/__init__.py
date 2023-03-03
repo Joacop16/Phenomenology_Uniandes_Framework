257a09dacb23053fb0c5b0c9e6b6d932de2ae0e9
Joaquin <j.penuela@uniandes.edu.co>
2023-03-03 17:03:40 -0500

    We fix init.py

--- ml_tools/__init__.py
+++ ml_tools/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "0.0.1"
 
-from ml_tools.log_reg_classifier import Log_Reg_Classifier
-from ml_tools.xgb_classifier import XGB_Classifier
+from Uniandes_Framework.ml_tools.log_reg_classifier import Log_Reg_Classifier
+from Uniandes_Framework.ml_tools.xgb_classifier import XGB_Classifier
