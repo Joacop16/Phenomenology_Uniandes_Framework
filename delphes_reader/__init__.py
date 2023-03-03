db5fed5e16b2e8f0dc30684b17c827fe0ddcc109
Joaquin <j.penuela@uniandes.edu.co>
2023-03-03 17:03:23 -0500

    I fix __init__ file, it did not have Uniandes_Framework. I also added two functions to root_analysis in order to save histograms in .root files and .txt files respectively. These functions are being using in ml_tools/bc_tools

--- delphes_reader/__init__.py
+++ delphes_reader/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.0.1"
 
-from delphes_reader.loader import DelphesLoader
-from delphes_reader.root_analysis import Quiet
+from Uniandes_Framework.delphes_reader.loader import DelphesLoader
+from Uniandes_Framework.delphes_reader.root_analysis import Quiet
 
